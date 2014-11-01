498efcdfd4616259550081be79f0388ea9867f55
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-11-01 10:18:40 +0200

    Initi commit.

---
+++ plugins/simplebox/simplebox.js
@@ -0,0 +1,49 @@
+(function($) {
+  Drupal.wysiwyg.plugins.simplebox = {
+     attach : '',
+  },
+
+  simpleboxAddPlugin = {
+    requires: 'widget',
+
+    icons: 'simplebox',
+
+    init: function( editor ) {
+        editor.ui.addButton('simplebox', {
+          label: 'Add simplebox',
+          command: 'simplebox',
+          icon: '/pp/profiles/pp/modules/custom/pp_wysiwyg_widget/plugins/simplebox/icons/simplebox.png'
+        });
+        editor.widgets.add( 'simplebox', {
+
+            button: 'Create a simple box',
+
+            template:
+                '<div class="simplebox">' +
+                    '<h2 class="simplebox-title">Title</h2>' +
+                    '<div class="simplebox-content"><p>Content...</p></div>' +
+                '</div>',
+
+            editables: {
+                title: {
+                    selector: '.simplebox-title',
+                    allowedContent: 'br strong em'
+                },
+                content: {
+                    selector: '.simplebox-content',
+                    allowedContent: 'p br ul ol li strong em'
+                }
+            },
+
+            allowedContent:
+                'div(!simplebox); div(!simplebox-content); h2(!simplebox-title)',
+
+            requiredContent: 'div(simplebox)',
+
+            upcast: function( element ) {
+                return element.name == 'div' && element.hasClass( 'simplebox' );
+            }
+        } );
+    }
+  }
+})(jQuery);
